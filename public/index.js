4731a27a23bca91a51539b1599d036537e2eccc7
RoelYoon <roelyoon2@gmail.com>
2024-03-12 18:28:21 +0900

    Path edited

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
-import * as THREE from '../node_modules/three';
-import { OrbitControls } from '../node_modules/three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from '../node_modules/three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
