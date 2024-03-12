d1865446bb52bfc2e046b7836950928b0f0eb9ea
RoelYoon <roelyoon2@gmail.com>
2024-03-12 14:55:15 +0900

    Server set up

---
+++ public/index.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
