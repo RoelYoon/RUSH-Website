943bc2c5513276ec650150ffdb89a3958fd18efa
RoelYoon <roelyoon2@gmail.com>
2024-03-18 18:36:01 +0900

    Updated

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.all('*',(req,res)=>{
     res.send("<h1>404 boiii</h1>").status(404);
 });
 
-app.listen(8082, "10.138.0.2", ()=>{console.log("Listening at IP 35.203.145.230 port 8082")});
+app.listen(8087, "10.138.0.2", ()=>{console.log("Listening at IP 35.203.145.230 port 8082")});
